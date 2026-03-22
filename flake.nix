fdc3a156bab5fd9e80965dd3ddc29471b9afe212
kushagharahi <3326002+kushagharahi@users.noreply.github.com>
2026-03-21 22:28:23 -0500

    Vulkan

--- flake.nix
+++ flake.nix
@@ -21,7 +21,8 @@
 
     llama-amd =
       (pkgs.llama-cpp.override {
-        rocmSupport = true;
+        rocmSupport = false;
+        vulkanSupport = true;
       }).overrideAttrs (oldAttrs: {
         src = llama-cpp-repo;
 
@@ -33,18 +34,15 @@
         nativeBuildInputs = pkgs.lib.filter (
           p:
             !(p ? pname && (p.pname == "nodejs" || p.pname == "npm-config-hook"))
-        ) (oldAttrs.nativeBuildInputs or []);
+        ) (oldAttrs.nativeBuildInputs or [pkgs.shaderc]);
 
         cmakeFlags =
           oldAttrs.cmakeFlags
           ++ [
-            "-DAMDGPU_TARGETS=gfx1030" # rt6800xt architecture aka RDNA2
-            "-DGGML_HIP=ON"
-            "-DGGML_HIP_ROCWMMA=ON" # rocWMMA (Radeon Open Compute Wavefront Matrix Multiply-Accumulate)
-            "-DGGML_HIP_ROCWMMA_FATTN=OFF" # flash attention. Not for RDNA2 :(
-            "-DCMAKE_HIP_ARCHITECTURES=gfx1030" # rt6800xt architecture aka RDNA2
-            "-DCMAKE_HIP_FLAGS=-I${pkgs.rocmPackages.rocwmma}/include"
             "-DCMAKE_BUILD_TYPE=Release"
+            "-DGGML_VULKAN=ON"
+            "-DGGML_VULKAN_PERF_PROFILING=OFF"
+            "-DGGML_NATIVE=ON" # Optimizes for your specific CPU as well
             # Wont work on official llama.cpp until https://github.com/ggml-org/llama.cpp/pull/20158 is merged
             "-DLLAMA_BUILD_WEBUI=OFF" # Don't bundle webui
           ];
@@ -53,18 +51,9 @@
         buildInputs =
           oldAttrs.buildInputs
           ++ [
-            pkgs.rocmPackages.clr
-            pkgs.rocmPackages.hipblas
-            pkgs.rocmPackages.rocblas
-            pkgs.rocmPackages.rocwmma
+            pkgs.vulkan-headers # Provides Vulkan_INCLUDE_DIR
+            pkgs.vulkan-loader # Provides Vulkan_LIBRARY
           ];
-
-        # skip other architectures when building
-        preConfigure = ''
-          export GPU_TARGETS="gfx1030"
-          export AMDGPU_TARGETS="gfx1030"
-          echo "0000000" > COMMIT
-        '';
 
         appendRunpaths = ["${placeholder "out"}/lib"];
       });
@@ -76,7 +65,7 @@
         pkgs.curl
       ];
 
-      shellHook = "source ./run.sh";
+      shellHook = "source ./run27b.sh";
     };
   };
 }
